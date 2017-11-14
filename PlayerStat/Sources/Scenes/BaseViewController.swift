f5aa5c11e1fcc5c59e000258d1ec25c7cd1acc4e
haisergeant <haisergeant@gmail.com>
2017-11-15 01:48:23 +1100

    clean cycle for match view and player view

---
+++ PlayerStat/Sources/Scenes/BaseViewController.swift
@@ -0,0 +1,56 @@
+//
+//  BaseViewController.swift
+//  PlayerStat
+//
+//  Created by Le Thanh Hai on 11/14/17.
+//  Copyright © 2017 haile. All rights reserved.
+//
+
+import UIKit
+
+class BaseViewController: UIViewController {
+    
+    init() {
+        super.init(nibName: nil, bundle: nil)
+        self.initialize()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func initialize() { }
+    func navigationTitle() -> String { return "" }
+    
+    func shouldShowNavigationBar() -> Bool {
+        return false
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.title = self.navigationTitle()
+        self.view.backgroundColor = .white
+        self.configureSubviews()
+        self.configureLayout()
+        self.configureContent()
+        self.configureStyle()
+        self.configureActions()
+    }
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        self.navigationController?.setNavigationBarHidden(!self.shouldShowNavigationBar(), animated: false)
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    func configureSubviews() { }
+    func configureLayout() { }
+    func configureContent() { }
+    func configureStyle() { }
+    func configureActions() { }
+}
+
---
+++ PlayerStat/Sources/URLConstants.swift
@@ -0,0 +1,15 @@
+//
+//  URLConstants.swift
+//  PlayerStat
+//
+//  Created by Le Thanh Hai on 11/14/17.
+//  Copyright © 2017 haile. All rights reserved.
+//
+
+import Foundation
+
+struct URL {
+    static let MATCH_URL = "https://statsapi.foxsports.com.au/3.0/api/sports/league/matches/NRL20172101/topplayerstats.json;type=fantasy_points;type=tackles;type=runs;type=run_metres?limit=5&userkey=A00239D3-45F6-4A0A-810C-54A347F144C2"
+    
+    static let PLAYER_URL = "https://statsapi.foxsports.com.au/3.0/api/sports/league/series/1/seasons/115/teams/%@/players/%@/detailedstats.json?&userkey=9024ec15-d791-4bfd-aa3b-5bcf5d36da4f"    
+}
